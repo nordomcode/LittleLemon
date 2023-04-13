9c99a70db7bcb42105a7ea513f146995a2036d87
nordomecode <dumpyreg@gmail.com>
2023-04-13 11:23:42 +1000

    add rest_framework, add serializers, add DRF APIView class

--- Restaraunt/urls.py
+++ Restaraunt/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, MenuItemsView, SingleMenuItemView
 
 urlpatterns = [
     path('', index, name='index'),
+    path('menu', MenuItemsView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
 ]
